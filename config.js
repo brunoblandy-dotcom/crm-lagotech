18e6711181f9b4c2fdc4837c9e900e8702388444
brunoblandy-dotcom <brunoblandy@gmail.com>
2026-03-13 10:59:28 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  supabase: {
+    url: 'https://pjejmlhxqvsiufhyvgfz.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqZWptbGh4cXZzaXVmaHl2Z2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNjkyMzEsImV4cCI6MjA4ODk0NTIzMX0.QKfwC7Jl3R2_dhdJ1rueSQJsMMJ5BiwtcCM-hL5wA8M'
+  }
+};
