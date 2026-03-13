41bf55dd97e056b0a2772a084809987a4b82e6b9
brunoblandy-dotcom <brunoblandy@gmail.com>
2026-03-13 10:59:54 -0300

    Delete ai_studio_code.js

--- ai_studio_code.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-  supabase: {
-    url: 'https://pjejmlhxqvsiufhyvgfz.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqZWptbGh4cXZzaXVmaHl2Z2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNjkyMzEsImV4cCI6MjA4ODk0NTIzMX0.QKfwC7Jl3R2_dhdJ1rueSQJsMMJ5BiwtcCM-hL5wA8M'
-  }
-};
